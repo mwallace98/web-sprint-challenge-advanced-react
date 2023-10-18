2b9b8671fcee5742a6a3ef8e0c7bdcd270cb166a
mwallace98 <mattwallace140@gmail.com>
2023-10-17 21:34:38 -0400

    Number of Steps Updates

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 // Suggested initial states
 const initialMessage = ''
@@ -16,16 +17,24 @@ const [steps,setSteps] = useState(initialSteps)
 const [email,setEmail] = useState(initialEmail)
 const [currentIndex,setCurrentIndex] = useState(initialIndex)
 
-  function getXY() {
-    const x = currentIndex % 3 + 1
-    const y = Math.floor(currentIndex / 3) + 1
 
-    return ` Coordinates (${x}, ${y})`
+  function getXY() {
+    const x = (currentIndex % 3) + 1
+    // const y = Math.floor(currentIndex / 3) + 1
+    let y
+    if(currentIndex < 3) y = 1
+    else if (currentIndex < 6) y = 2
+    else if(currentIndex < 9) y = 3
+
+    return [x,y]
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
   }
 
   function getXYMessage() {
+    const [x,y] = getXY()
+
+    return ` Coordinates (${x}, ${y})`
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
@@ -35,16 +44,20 @@ const [currentIndex,setCurrentIndex] = useState(initialIndex)
     setSteps(initialSteps)
     setEmail(initialEmail)
     setCurrentIndex(initialIndex)
+    
     // Use this helper to reset all states to their initial values.
   }
 
   function getNextIndex(direction) {
+    setSteps(steps + 1)    
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+
   }
 
   function move(evt) {
+    setSteps(steps + 1)
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
@@ -54,20 +67,30 @@ const [currentIndex,setCurrentIndex] = useState(initialIndex)
   }
 
   function onSubmit(evt) {
+    evt.preventDefault()
+    const URL = 'http://localhost:9000/api/result'
+
+    axios.post(URL)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
     // Use a POST request to send a payload to the server.
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">{getXY()}</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">{getXYMessage()}</h3>
+        <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === currentIndex ? ' active' : ''}`}>
+              {idx === currentIndex ? 'B' : null}
             </div>
           ))
         }
@@ -76,15 +99,15 @@ const [currentIndex,setCurrentIndex] = useState(initialIndex)
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
+        <button id="left" onClick={getNextIndex}>LEFT</button>
+        <button id="up" onClick={move}>UP</button>
+        <button id="right" onClick={getNextIndex}>RIGHT</button>
+        <button id="down" onClick={move}>DOWN</button>
         <button id="reset" onClick={reset}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+        <input id="submit" type="submit" onClick={onSubmit}></input>
       </form>
     </div>
   )
