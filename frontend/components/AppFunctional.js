389281734e1e794e4bd8cf63fb67aacb665731b4
mwallace98 <mattwallace140@gmail.com>
2023-10-20 18:32:32 -0400

    All helper Functions working, only 60 tests passin

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -13,14 +13,14 @@ const initialIndex = 4 // the index the "B" is at
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
-const [steps,setSteps] = useState(initialSteps)
-const [email,setEmail] = useState(initialEmail)
-const [currentIndex,setCurrentIndex] = useState(initialIndex)
+const [steps,setSteps] = useState(0)
+const [email,setEmail] = useState('')
+const [currentIndex,setCurrentIndex] = useState(4)
+const [initialMessage,setInitialMessage] = useState('')
 
 
   function getXY() {
     const x = (currentIndex % 3) + 1
-    // const y = Math.floor(currentIndex / 3) + 1
     let y
     if(currentIndex < 3) y = 1
     else if (currentIndex < 6) y = 2
@@ -44,23 +44,28 @@ const [currentIndex,setCurrentIndex] = useState(initialIndex)
     setSteps(initialSteps)
     setEmail(initialEmail)
     setCurrentIndex(initialIndex)
+    setInitialMessage('')
     
     // Use this helper to reset all states to their initial values.
   }
 
   function getNextIndex(direction) {
       
-      if(direction.target.id ==='left'){
-        setCurrentIndex(currentIndex - 1)
-        //how do i stop it from going past 0 and over 8???
-      } else if (direction.target.id === 'right'){
-        setCurrentIndex(currentIndex + 1)
-      }else if(direction.target.id === 'down'){
-        setCurrentIndex(currentIndex + 3)
-      }
-      else if (direction.target.id === 'up'){
-        setCurrentIndex(currentIndex - 3)
-      }
+    const x = currentIndex % 3;
+    const y =Math.floor(currentIndex / 3);
+
+    switch(direction){
+      case 'left':
+        return x > 0 ? currentIndex - 1 : currentIndex;
+        case 'up':
+          return y > 0 ? currentIndex - 3 : currentIndex;
+        case'right':
+          return x < 2 ? currentIndex + 1 : currentIndex;
+        case 'down':
+          return y < 2 ? currentIndex + 3 : currentIndex;
+        default:
+          return currentIndex
+    }
     
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
@@ -69,25 +74,52 @@ const [currentIndex,setCurrentIndex] = useState(initialIndex)
   }
 
   function move(evt) {
-    setSteps(steps + 1)
+    const direction = evt.target.id;
+    const nextIndex = getNextIndex(direction);
+
+    if (nextIndex !== currentIndex){
+      setSteps(steps + 1);
+      setCurrentIndex(nextIndex);
+      setInitialMessage('')
+    } else if(direction === 'left'){
+      setInitialMessage("You can't go left")
+    }else if(direction === 'right'){
+      setInitialMessage("You can't go right")
+    }else if (direction === 'up'){
+      setInitialMessage("You can't go up")
+    }else if(direction === 'down'){
+      setInitialMessage("You can't go down")
+    }
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
 
   function onChange(evt) {
+    const newEmail = evt.target.value
+    setEmail(newEmail)
     // You will need this to update the value of the input.
   }
 
   function onSubmit(evt) {
     evt.preventDefault()
+    reset()
+    const [x,y] = getXY()
     const URL = 'http://localhost:9000/api/result'
 
-    axios.post(URL)
+    axios.post(URL, {
+      email: email,
+      steps: steps,
+      x: x,
+      y: y
+    })
     .then(res => {
       console.log(res)
+      setInitialMessage(res.data.message)
     })
     .catch(err => {
       console.log(err)
+      setInitialMessage("Ouch: steps is required")
+      
     })
     // Use a POST request to send a payload to the server.
   }
@@ -108,18 +140,18 @@ const [currentIndex,setCurrentIndex] = useState(initialIndex)
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{initialMessage}</h3>
       </div>
       <div id="keypad">
-        <button id="left" onClick={getNextIndex}>LEFT</button>
-        <button id="up" onClick={getNextIndex}>UP</button>
-        <button id="right" onClick={getNextIndex}>RIGHT</button>
-        <button id="down" onClick={getNextIndex}>DOWN</button>
+        <button id="left" onClick={move}>LEFT</button>
+        <button id="up" onClick={move}>UP</button>
+        <button id="right" onClick={move}>RIGHT</button>
+        <button id="down" onClick={move}>DOWN</button>
         <button id="reset" onClick={reset}>reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit" onClick={onSubmit}></input>
+        <input id="email" type="email" placeholder="type email" value ={email} onChange={onChange}></input>
+        <input id="submit" type="submit" onClick={onSubmit}  ></input>
       </form>
     </div>
   )
