a57bb19cdccae3ecd746d8374c89f97205053561
mwallace98 <mattwallace140@gmail.com>
2023-10-22 17:59:53 -0400

    All tasks completed

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,10 +1,11 @@
 import AppFunctional from "./AppFunctional"
 import {render,screen} from '@testing-library/react'
+import React from "react"
 
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
 
 test('renders without error', () => {
   render (<AppFunctional />)
